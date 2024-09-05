617de6ac7d63e48cf689890c0a365204d860a765
HruntRV <161721433+HruntRV@users.noreply.github.com>
2024-09-06 00:45:22 +0300

    Update settings.py

--- diploma/settings.py
+++ diploma/settings.py
@@ -77,7 +77,8 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'diploma.wsgi.application'
 
-#STATIC_URL = '/static/'
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
